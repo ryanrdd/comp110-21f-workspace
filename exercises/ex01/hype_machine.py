b194bf39899c2a65688d8f31d271f63f82f2768a
Ryan Diosdado <ryanrd@live.unc.edu>
2021-08-26 17:19:59 -0400

    final version hype machine

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -3,9 +3,7 @@
 __author__ = "730314539"
 
 name: str = input("What is your name? ")
-print("You entered: ")
-print(name)
-
-print(name+ ", you have a great name")
-print("Have you seen the news?")
-input("YES OR NO? ")
+print(name + ", it is great to meet you.")
+print("Are you familiar with C110, " + name + "?")
+print("I just started the class! ")
+print("Thank you, " + name + ", for your time. ")
