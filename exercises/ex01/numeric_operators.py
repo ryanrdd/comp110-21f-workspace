9496683f6492a19136125bf84b7783d47ef9d5f8
Ryan Diosdado <ryanrd@live.unc.edu>
2021-08-31 09:42:12 -0400

    ex1

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -2,18 +2,18 @@
 
 __author__ = "730314539"
 
-input1: str = (input("Left-hand side: "))
-input2: str = (input("Right-hand side: "))
+input_1: str = (input("Left-hand side: "))
+input_2: str = (input("Right-hand side: "))
 
-int1: int = int(input1)
-int2: int = int(input2)
+int_1: int = int(input_1)
+int_2: int = int(input_2)
 
-square_int: int = (int1**int2)
-divide_int: float = (int1 / int2)
-integer_int: int = (int1 // int2)
-modulo_int: int = (int1 % int2)
+square_int: int = (int_1**int_2)
+divide_int: float = (int_1 / int_2)
+integer_int: int = (int_1 // int_2)
+modulo_int: int = (int_1 % int_2)
 
-print(input1 + " ** " + input2 + " is " + str(square_int))
-print(input1 + " / " + input2 + " is " + str(divide_int))
-print(input1 + " // " + input2 + " is " + str(integer_int))
-print(input1 + " % " + input2 + " is " + str(modulo_int))
+print(input_1 + " ** " + input_2 + " is " + str(square_int))
+print(input_1 + " / " + input_2 + " is " + str(divide_int))
+print(input_1 + " // " + input_2 + " is " + str(integer_int))
+print(input_1 + " % " + input_2 + " is " + str(modulo_int))
