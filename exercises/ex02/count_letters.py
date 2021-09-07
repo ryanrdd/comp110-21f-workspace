aa93aebe671eb021709ec2bb86722f2d5e7507b3
Ryan Diosdado <ryanrd@live.unc.edu>
2021-09-06 21:41:38 -0400

    check

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -1,6 +1,21 @@
 """Counting letters in a string."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730314539"
 
+from random import randint
 
-# Begin your solution here...
+fortune: int = int(randint(1,4))
+
+print("Your fortune cookie says...")
+if fortune == 1:
+    print("Your luckiest day will happen after a bleak one!")
+else:
+    if fortune == 2:
+        print("A favor from a friend will be called upon soon.")
+    else:
+        if fortune == 3:
+            print("Your true love will be found only if your eyes are open.")
+        else:
+            if fortune == 4:
+                print("Flowers and rainbows often come because of rain.")
+print("Now, go spread positive vibes!")
