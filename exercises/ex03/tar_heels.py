566bc20df673b5e2e9a6608a5b3901e55b0e0c29
Ryan Diosdado <ryanrd@live.unc.edu>
2021-09-12 17:55:19 -0400

    wasd

--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,19 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730314539"
 
+guess: str = input("Enter an int: ")
+both: int = 0
 
-# Begin your solution here...
+if int(guess) % 2 == 0:
+    both = 1 + both
+if int(guess) % 7 == 0:
+    both = 2 + both
+if both == 3:
+    print("TAR HEELS")
+if both == 2:
+    print("HEELS")
+if both == 1:
+    print("TAR")
+if both == 0:
+    print("CAROLINA")
