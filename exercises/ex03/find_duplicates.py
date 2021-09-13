9fa599ca610d34e4270d5256d15c45dd7969f0a5
Ryan Diosdado <ryanrd@live.unc.edu>
2021-09-13 17:55:20 -0400

    wasd

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,21 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730314539"
+
+word: str = input("Enter a word: ")
+
+i: int = 0
+j: int = 0
+count: bool = False
+
+while i < len(word):
+    while j < len(word):
+        print("i" + str(i))
+        print("j" + str(j))
+        if i != j:
+            if word[i] == word[j]:
+                count = True
+        j = j + 1
+    i = i + 1
+
+print("Found duplicate: " + str(count))
