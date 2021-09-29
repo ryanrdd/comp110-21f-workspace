edf60686e4d348f0b02305b4079021ca7cff6f23
Ryan Diosdado <ryanrd@live.unc.edu>
2021-09-28 21:15:39 -0400

    commit

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -4,7 +4,7 @@ __author__ = "730314539"
 
 
 def all(sequence: list[int], number: int) -> bool:
-    """all list implementation"""
+    """All list implementation!"""
     i: int = 0
     if len(sequence) >= 1:
         while i < len(sequence):
@@ -19,7 +19,7 @@ def all(sequence: list[int], number: int) -> bool:
 
 
 def is_equal(l_1: list[int], l_2: list[int]) -> bool:
-    """is_equal list boolean"""
+    """Is equal list boolean!"""
     f: int = 0
     if len(l_1) != len(l_2):
         return False   
@@ -31,10 +31,11 @@ def is_equal(l_1: list[int], l_2: list[int]) -> bool:
 
     return True
 
+
 def max(input: list[int]) -> int:
-    """Maximum Value in a list of integers"""
+    """Maximum Value in a list of integers!"""
     k: int = 0
-    value: int = 0
+    value: int = input[0]
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     else:
