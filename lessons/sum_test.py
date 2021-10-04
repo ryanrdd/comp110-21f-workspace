191468e5fa89b8c7a51083c1e2b8258693817020
Ryan Diosdado <ryanrd@live.unc.edu>
2021-10-03 21:35:45 -0400

    commit

--- lessons/sum_test.py
+++ lessons/sum_test.py
@@ -17,3 +17,7 @@ def test_sum_single_item() -> None:
 def test_sum_many_items() -> None:
     xs: list[float] = [1.0, 2.0, 3.0]
     assert sum(xs) == 6.0
+
+
+def test_sum_many_items_again() -> None:
+    assert sum([-1.0, 1.0, -2.0, 2.0]) == 0.0
---
+++ sandbox/messing_around.py
@@ -0,0 +1,24 @@
+"""Game maybe"""
+
+# gacha game
+# core mechanics:
+    # user posesses cards
+        # cards are associated with characters
+            # characters: 
+        # characters have power level
+        # higher power level, more dmg
+    # map
+        # multiple selection map
+        # options to go into levels
+        # give three options of different rewards
+    # levels
+        # choice between limited and unlimited levels
+            # limited: normal levels
+            # unlimited: challenge levels
+    # user space
+        # users can fight other users and see each others users and characters
+        # competitive and ladder system
+    # map contains 20 levels per battle pass season
+        # 1 season lasts 4-6 months
+    # battle pass
+        # battle pass contains 100 levels with rewards similar to COD
