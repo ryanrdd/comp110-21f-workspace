1417b355a50df8685944032be029733c8771076d
Ryan Diosdado <ryanrd@live.unc.edu>
2021-10-20 14:37:38 -0400

    commit

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -2,14 +2,15 @@
 
 __author__ = "730314539"
 
-def invert(a: dict[str, str],b: dict[str, str]) -> dict[str, str]:
+def invert(a: dict[str, str]) -> dict[str, str]:
     """Code that inverts the keys and values of the two lists"""
     result: dict[str, str] = {}
-    if a and b == dict[str, str]:
-        for key in a:
-            result[key] = b[key]
-        for key in b:
-            result[key] = a[key]
-        return result
-    else:
-        raise KeyError("KeyError")
+    for key in a:
+        k: str = key
+        v: str = a.pop(k)
+        result[k] = v
+    # for value in a:
+    #     b: str = value
+    #     c: str = a.pop(b)
+    #     result[b] = c
+    return result
