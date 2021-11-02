7a72c33bae25575940025b729e8d8f768260853f
Ryan Diosdado <ryanrd@live.unc.edu>
2021-11-01 20:03:51 -0400

    ocm]

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -1,5 +1,16 @@
 """Utility functions."""
 
-__author__ = "123456789"
+__author__ = "730314539"
 
-# Define your functions below
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read an entire CSV of data into a list of rows, each row represented as dict[str,str]"""
+    result: list[dict[str,str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+    file_handle.close()
+    return result
