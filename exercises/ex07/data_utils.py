6533ec0e62d9025e7d4fdeb96d870459ae34a909
Ryan Diosdado <ryanrd@live.unc.edu>
2021-11-02 18:48:59 -0400

    waod

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -4,7 +4,6 @@ __author__ = "730314539"
 
 from csv import DictReader
 
-
 def read_csv_rows(filename: str) -> list[dict[str, str]]:
     """Read an entire CSV of data into a list of rows, each row represented as dict[str,str]."""
     result: list[dict[str,str]] = []
@@ -31,4 +30,23 @@ def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
     first_row: dict[str, str] = row_table[0]
     for column in first_row:
         result[column] = column_values(row_table, column)
+    return result
+
+
+def head(column_table: dict[str, list[str]], N: int) -> dict[str, list[str]]:
+    """Produce a new column-based table with only the first `N` rows of data for each column."""
+    result: dict[str, list[str]] = {}
+    for key in column_table:
+        first_n: list[str] = []
+        for i in range(N):
+            first_n.append(column_table[key][i])
+        result[key] = first_n
+    return result
+
+
+def selected(column_table: dict[str, list[str]], column_names: list[str]) -> dict[str, list[str]]:
+    """Produce a new column-based table with only a specific subset of the original columns."""
+    result: dict[str, list[str]] = {}
+    for s in column_names:
+        result[s] = column_table[s]
     return result
