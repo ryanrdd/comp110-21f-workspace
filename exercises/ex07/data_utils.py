ef85cf3d66c134a2959bf552108166830862d782
Ryan Diosdado <ryanrd@live.unc.edu>
2021-11-02 10:34:07 -0400

    cowm

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -6,11 +6,29 @@ from csv import DictReader
 
 
 def read_csv_rows(filename: str) -> list[dict[str, str]]:
-    """Read an entire CSV of data into a list of rows, each row represented as dict[str,str]"""
+    """Read an entire CSV of data into a list of rows, each row represented as dict[str,str]."""
     result: list[dict[str,str]] = []
     file_handle = open(filename, "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
     for row in csv_reader:
         result.append(row)
     file_handle.close()
+    return result
+
+
+def column_values(table: list[dict[str,str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column whose name is in the second paramter."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
     return result
